31c66749f717cf1448c6f6562f6b2362e0b20392
Adi Baswendra <cah.sibat@gmail.com>
2020-06-15 18:19:35 +0700

    feature-add-user-upload-cover

--- src/views/User.js
+++ src/views/User.js
@@ -4,6 +4,9 @@ import axios from 'axios'
 const User = () => {
 
 	const inputFile = useRef(null);
+	const inputCoverFile = useRef(null);
+	const imagePhoto = useRef(null);
+	const imageCover = useRef(null);
 	const [data,setData] = useState([])
 	const [accesstoken,setAccessToken] = useState(null)
 	const [file,setFile] = useState(null)
@@ -32,9 +35,42 @@ const User = () => {
 
 	const uploadpicture = (e) => {
 		e.preventDefault()
-		// console.log(file)
 		inputFile.current.click()
 	}
+
+	const onCoverChange = (e) => {
+		let formData = new FormData();
+		formData.append('image', e.target.files[0]); 
+		axios({
+		  	method: 'post',
+		  	data: formData,
+		  	headers: { 
+		  		'Content-Type': 'application/x-www-form-urlencoded' , 
+		  		'Accept': 'application/json',
+		  		'Access-Control-Allow-Origin' : '*',
+	  			'Authorization' : accesstoken
+		  	},
+		  	url: process.env.REACT_APP_API_BASE_URL+'api/v1/uploads/cover'
+		})
+	  	.then(function (response) {
+	  		console.log(response)
+		    if(response.status === 201){
+		    	let url = response.data.data.user_picture.cover_picture.url
+		    	imageCover.current.src = url
+		    }else{
+		    	console.log(response.data.data.error.errors)
+		    }
+	  	})
+	  	.catch(function (error1) {
+	  		console.log(error1)
+	  		// alert(error1.response.data.error.errors.join('\n'))
+	  	});
+	}
+
+	const onClickCover = (e) => {
+		e.preventDefault()
+		inputCoverFile.current.click();
+	} 
 
 	const onUploadChange = (e) => {
 		let formData = new FormData();
@@ -84,12 +120,15 @@ const User = () => {
 			<div className="row">
 				<div className="col-md-12">
 					<div className="profile-cover">
-						<img className="img-fluid" src={ data.user.cover_picture.url === null ? "https://via.placeholder.com/800x200" : data.user.cover_picture.url} />
+						<input type="file" ref={inputCoverFile} className="d-none" onChange={ onCoverChange }  />
+						<a href="#" onClick={onClickCover}>
+						<img ref={imageCover} className="img-fluid" src={ data.user.cover_picture.url === null ? "https://via.placeholder.com/1110x200" : data.user.cover_picture.url} />
+						</a>
 					</div>
 					<div className="profile-photo">
 						<input type="file" ref={inputFile} className="d-none" onChange={ onUploadChange }  />
 						<a href="#" onClick={uploadpicture}>
-							<img className="img-fluid" src={data.user.user_picture === null ? "https://via.placeholder.com/150" : data.user.user_picture.picture.url } />
+							<img ref={imagePhoto}  className="img-fluid" src={data.user.user_picture === null ? "https://via.placeholder.com/150" : data.user.user_picture.picture.url } />
 						</a>
 					</div>
 				</div>
